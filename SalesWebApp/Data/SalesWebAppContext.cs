0daa957a485c134460c5752f6dd0bb163fcc8297
Lavinia Fonte <lavifonte@hotmail.com>
2022-04-03 05:37:41 -0300

    add other entities and make second migration

--- SalesWebApp/Data/SalesWebAppContext.cs
+++ SalesWebApp/Data/SalesWebAppContext.cs
@@ -14,6 +14,9 @@ namespace SalesWebApp.Data
         {
         }
 
-        public DbSet<SalesWebApp.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> Sales { get; set; }
     }
 }
