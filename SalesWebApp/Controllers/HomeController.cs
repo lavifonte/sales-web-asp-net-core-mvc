601d5f3194c43efcdf4324806a28f47a6613f61d
Lavinia Fonte <lavifonte@hotmail.com>
2022-04-03 04:49:32 -0300

    'department' CRUD scaffold

--- SalesWebApp/Controllers/HomeController.cs
+++ SalesWebApp/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using SalesWebApp.Models.ViewModels;
 
 namespace SalesWebApp.Controllers
 {
@@ -17,7 +18,7 @@ namespace SalesWebApp.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "sales web application created using asp.net core mvc";
 
             return View();
         }
