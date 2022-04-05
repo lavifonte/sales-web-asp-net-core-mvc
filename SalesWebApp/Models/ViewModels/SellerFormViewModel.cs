e516018363a2a6d4055a9bcbbfd133cf855f90e7
Lavinia Fonte <lavifonte@hotmail.com>
2022-04-05 02:05:52 -0300

    sellerFormViewModel and select component

---
+++ SalesWebApp/Models/ViewModels/SellerFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebApp.Models.ViewModels
+{
+    public class SellerFormViewModel
+    {
+    }
+}
